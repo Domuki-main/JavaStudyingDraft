98a1b9c76da8333f27406a32ebd2081506e090b8
DOMUKI <59246869+Domuki-main@users.noreply.github.com>
2020-02-09 19:28:22 +0800

    Update tryCatchLoop.java

--- tryCatchLoop.java
+++ tryCatchLoop.java
@@ -1,4 +1,3 @@
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -22,20 +21,11 @@ public class tryCatchLoop {
 
             } catch (InputMismatchException e) {
 
-                //e.getMessage();
-
-                /*
-                 * e.toString();             获得异常种类和错误信息
-                 *
-                 * e.getMessage();           获得错误信息
-                 *
-                 * e.printStackTrace();      在控制台打印出异常种类，错误信息和出错位置等
-                 */
-
                 System.out.println("not available");
 
                 sc = new Scanner(System.in);
-                //扫描器在输入内容按回车键时,系统默认在输入内容后面加了(\n 或\r),不重新创建扫描器对象的话会陷入死循环
+                //when Scanner is in inputing situation, the system'll add "\n" or "\r" in the end of the content when pressing the Enter key.
+                //if don't create new Scanner, the program will become endless loop...
 
             } finally {
 
